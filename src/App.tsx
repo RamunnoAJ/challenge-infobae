1b6cbedc849ef108735b015facfbebfb1d37c125
RamunnoAJ <ramunnoagustin@gmail.com>
2025-02-24 00:43:57 +0000

    commit de inicio

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className="text-xl text-red-500">Inicio</h1>;
+}
+
+export default App;
---
+++ src/context/auth-context.tsx
@@ -0,0 +1,56 @@
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react";
+import { auth, provider } from "../config/firebase";
+import {
+  onAuthStateChanged,
+  signOut,
+  User,
+  signInWithPopup,
+} from "firebase/auth";
+
+type AuthContextType = {
+  user: User | null;
+  loginWithGoogle: () => void;
+  logout: () => void;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const loginWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      setUser(result.user);
+    } catch (error) {
+      console.error("Google Sign-In Error:", error);
+    }
+  };
+
+  const logout = () => signOut(auth);
+
+  return (
+    <AuthContext.Provider value={{ user, loginWithGoogle, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("useAuth must be used within an AuthProvider");
+  return context;
+};
